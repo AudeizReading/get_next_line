e874c36badef4813766c9e30b91f786a571d6e48
AudeizReading <aaericia@hotmail.fr>
2021-05-18 12:26:29 +0200

    working on the searching of \n in the buffer read or readout you

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: alellouc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/12 08:54:45 by alellouc          #+#    #+#             */
-/*   Updated: 2021/05/13 14:55:25 by alellouc         ###   ########.fr       */
+/*   Updated: 2021/05/18 12:25:14 by alellouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,46 +50,98 @@ char	*ft_strdup(const char *s1)
 	return (dest);
 }
 
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*p_s;
+	unsigned char	c_c;
+
+	p_s = (unsigned char *)s;
+	c_c = (unsigned char)c;
+	while (n--)
+		if (*p_s++ == c_c)
+			return (--p_s);
+	return (NULL);
+}
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	unsigned char		*p_dst;
+	unsigned const char	*p_src;
+
+	p_dst = dst;
+	p_src = src;
+	if (!p_dst && !p_src)
+		return ((void *)0);
+	while (n--)
+		*p_dst++ = *p_src++;
+	return (dst);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*dst;
+	size_t	len_s1;
+	size_t	len_s2;
+/*	size_t	len_tmp;*/
+
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	dst = (char *)malloc(sizeof(*dst) * (len_s1 + len_s2 + 1));
+	if (dst == NULL)
+		return (NULL);
+/*	len_tmp = len_s1;
+	while (len_tmp--)
+		dst[len_tmp] = s1[len_tmp];
+	len_tmp = len_s2;
+	while (len_tmp--)
+		dst[len_s1 + len_tmp] = s2[len_tmp];*/
+	ft_memcpy(dst, s1, len_s1);
+	ft_memcpy((dst + len_s1), s2, len_s2);
+	dst[len_s1 + len_s2] = 0;
+	return (dst);
+}
+
 int	get_next_line(int fd, char **line)
 {
 	int	ret;
 	static char buf[BUFFER_SIZE + 1];
 
-	/*if (ft_strlen(buf) >= BUFFER_SIZE + 1)
-		ret = read(fd, &buf[ft_strlen(buf)], BUFFER_SIZE - ft_strlen(buf));
-	else*/
+	*line = "";
+	while (!ft_memchr(buf, '\n', ft_strlen(buf)))
+	{
 		ret = read(fd, buf, BUFFER_SIZE);
+		*line = ft_strjoin(*line, buf);
+	}
 	/*printf("\nret = %d", ret);*/
 /*	printf(" ret = %d\n", ret);*/
 	if (ret > 0)
 		ret = 1;
 	if (ret == 0)
 		*buf = 0;
-	/*if (ret == BUFFER_SIZE)
-		get_next_line(fd, line);*/
-	*line = ft_strdup(buf);
+	/**line = ft_strdup(buf);*/
+/*	*line = ft_strjoin(*line, buf);*/
 	return (ret);
 }
 
 /* Uniquement pour le debugage */
 int	main(int argc, char **argv)
 {
-	(void) argc;
-	(void) argv;
 	char	*line;
-	int		fd;
+	size_t	fd;
 	int		gnl;
 
-/*	fd = open("test_file.txt", O_RDONLY);*/
-	fd = 0;
-	gnl = 1;
+	if (argc == 2)
+		fd = open(argv[1], O_RDONLY);
+	else
+		fd = 0;
+/*	gnl = 1;
 	while (gnl == 1)
-	{
-		gnl = get_next_line(fd, &line);
-		ft_putstr_fd(line, 1);
+	{*/
+	gnl = get_next_line(fd, &line);
+	ft_putstr_fd(line, 1);
 	/*	ft_putstr_fd("\n", 1);*/
 	/*	printf(" gnl = %d\n", gnl);*/
-	}
+/*	}*/
 	free(line);
 	close(fd);
 	return (0);
