edd5497263584348f126628c4da5044d7a0be03a
AudeizReading <aaericia@hotmail.fr>
2021-05-23 20:29:08 +0200

    pq est ce qu'il a fallu que je detare mon home ici ? serieux ?

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: alellouc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/12 08:54:45 by alellouc          #+#    #+#             */
-/*   Updated: 2021/05/23 13:54:58 by alellouc         ###   ########.fr       */
+/*   Updated: 2021/05/23 20:20:02 by alellouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -122,6 +122,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 		return ((void *)0);
 	while (n--)
 		*p_dst++ = *p_src++;
+/*	free((void *)p_dst);
+	free((void *)p_src);*/
 	return (dst);
 }
 
@@ -130,23 +132,24 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*dst;
 	size_t	len_s1;
 	size_t	len_s2;
-/*	size_t	len_tmp;*/
+	size_t	len_tmp;
 
 	len_s1 = ft_strlen(s1);
 	len_s2 = ft_strlen(s2);
 	dst = (char *)malloc(sizeof(*dst) * (len_s1 + len_s2 + 1));
 	if (dst == NULL)
 		return (NULL);
-/*	len_tmp = len_s1;
+	len_tmp = len_s1;
 	while (len_tmp--)
 		dst[len_tmp] = s1[len_tmp];
 	len_tmp = len_s2;
 	while (len_tmp--)
-		dst[len_s1 + len_tmp] = s2[len_tmp];*/
-	ft_memcpy(dst, s1, len_s1);
-	ft_memcpy((dst + len_s1), s2, len_s2);
+		dst[len_s1 + len_tmp] = s2[len_tmp];
+/*ft_memcpy(dst, s1, len_s1);
+ft_memcpy((dst + len_s1), s2, len_s2);*/
 	dst[len_s1 + len_s2] = 0;
-	free((void *)s1);
+	if (ft_strlen(s1) > 0)
+		free((void *)s1);
 /*	free((void *)s2);*/ /* On l'utilisera si je me decide a utiliser buf comme un pointeur*/
 	return (dst);
 }
@@ -171,67 +174,109 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	return (dst);
 }
 
+char	*ft_strnew(size_t size)
+{
+	return (ft_calloc(sizeof(char), size + 1));
+}
+
+int		ft_memdel(void **ptr)
+{
+	if (*ptr)
+	{
+		ft_memset(*ptr, 0, ft_strlen(*ptr));
+		free(*ptr);
+		*ptr = NULL;
+		return (1);
+	}
+	return (0);
+}
+
 int	get_next_line(int fd, char **line)
 {
 	int			ret;
 /*	static char *buf = NULL;*/
-	char		buf[BUFFER_SIZE];
-/*	static char	*endline = NULL;*/
-	char		*newline;
+	char		buf[BUFFER_SIZE + 1];
+	/*static char	*endline = NULL;*/
+	static char	*newline;
+/*	char		*tmp;*/
 /*	int			i;*/
 
+/*	ssize_t		r;
+	char		bf[BUFFER_SIZE + (r = 1)];
+	static char	*c_line = NULL;
+	char		*tmp;
+
+	if (fd < 0 || !line || BUFFER_SIZE < 1)
+		return (-1);
+	c_line == NULL ? c_line = ft_strnew(0) : NULL;
+	while (!ft_strchr(c_line, '\n') && (r = read(fd, bf, BUFFER_SIZE)) > 0)
+	{
+		bf[r] = 0;
+		tmp = ft_strjoin(c_line, bf);
+		ft_memdel((void **)&c_line);
+		c_line = tmp;
+	}
+	if (r == 0)
+		*line = ft_strdup(c_line);
+	else if (r > 0)
+		*line = ft_substr(c_line, 0, ((char *)ft_strchr(c_line, '\n') - c_line));
+	else
+		return (-1);
+	tmp = ft_strdup(c_line + (ft_strlen(*line) + ((r > 0) ? +1 : +0)));
+	ft_memdel((void **)&c_line);
+	c_line = tmp;
+	return (r == 0 ? 0 * ft_memdel((void **)&c_line) : 1);*/
+	
 	if (fd < 0 || fd > FOPEN_MAX || !line || BUFFER_SIZE < 1)
 		return (-1);
-	newline = ft_calloc(sizeof(*newline), 1);
+	/*if (!newline)
+		newline = ft_calloc(sizeof(*newline), 1);
+	else
+		*line = ft_strdup(newline);*/
+	if (newline)
+		printf("\t\033[1;31mnewline - debut: %s\033[0m\n", newline);
 	buf[0] = 0;
-	while ((read(fd, buf, BUFFER_SIZE - 1)) > 0 && !(ft_strchr(buf, '\n')))
+	ret = 1;
+	while (!ft_strchr(newline, '\n') && read(fd, buf, BUFFER_SIZE) > 0)
 	{
-		printf("\033[1;31m\tbuf : %s\033[0m\n", buf);
+		buf[BUFFER_SIZE] = 0;
 		ret = ft_strlen(buf);
-		printf("\033[1;31m\tsize buf : %d\033[0m\n", ret);
-		buf[BUFFER_SIZE - 1] = 0;
 		newline = ft_strjoin(newline, buf);
-	/*	printf("\tnewline : %s\n", newline);
-		printf("\tbuf : %s\n", buf);*/
+
+	/*	printf("\033[1;31m\tbuf : %s\033[0m\n", buf);
+		printf("\033[1;31m\tsize buf : %d\033[0m\n", ret);
+		printf("\tnewline : %s\n", newline);*/
 	}
-	if (ret == 0)
+	if (ret == 0 || !ft_strlen(buf))
+	{
+	/*	*line = ft_strdup(newline);
+		free(newline);*/
 		return (0);
+	}
 	else if (ret == -1)
 		return (-1);
 	else
 	{
+		/* Scinder "buf" en deux parties :
+		** celle avant \n, qui doit aller dans line
+		** celle apres \n, qui doit aller dans la variable statique */
+		/*newline = ft_strjoin(newline, buf);*/
 		*line = ft_strdup(newline);
 		free(newline);
-		*line = ft_strjoin(*line, buf);
-		return (1);
+		newline = ft_strchr(buf, '\n');
+		if (newline)
+			printf("\t\033[1;31mnewline: %s\033[0m\n", newline);
+		printf("\n\t\033[1;35mbuf: %s\033[0m\n", buf);
+	/*	*line = ft_substr(newline, 0, ((char *)ft_strchr(newline, '\n') -
+	**	newline));*/
+	/*	return (1);*/
 	}
-
-	/*	if (fd < 0 || !line || BUFFER_SIZE < 1)
-		return (-1);
-	if (!endline)
-		endline = (char *)malloc(sizeof(*endline) * 1);
-	else
-		endline = NULL;
-	while (!ft_memchr(endline, '\n', ft_strlen(endline)) && (ret = read(fd, buf, BUFFER_SIZE)) > 0)
-	{
-		newline = ft_strjoin(endline, buf);
-		free(endline);
-		endline = NULL;
-		endline = newline;
-	}
-	if (ret == 0)
-		*line = ft_strdup(endline);
-	else if (ret > 0)
-		*line = ft_substr(endline, 0, (ft_memchr(endline, '\n', ft_strlen(endline)) - (void *)endline));
-	else
-		return (-1);
-	newline = ft_strdup(endline + (ft_strlen(*line) + ((ret > 0) ? +1 : +0)));
-	free(endline);
-	endline = NULL;
-	if (ret == 0)
-		return (0);
-	else
-		return (1);*/
+/*	tmp = ft_strdup(newline + (ft_strlen(*line) + ((r > 0) ? +1 : +0)));*/
+/*	ft_memdel((void **)&c_line);*/
+/*	free(newline);*/
+/*	newline = tmp;
+	free(tmp);*/
+	return (1);
 	
 /*	if (!line || BUFFER_SIZE < 1 || fd < 0)
 		return (-1);
@@ -313,15 +358,15 @@ int	main(int argc, char **argv)
 	char	*line;
 	size_t	fd;
 	int		gnl;
-	int		i = 0;
+/*	int		i = 0;*/
 
 	if (argc == 2)
 		fd = open(argv[1], O_RDONLY);
 	else
 		fd = 0;
 	gnl = 1;
-	/*while (gnl == 1)*/
-	while (i++ <= 15)
+	while (gnl == 1)
+/*	while (i++ <= 15)*/
 	{
 		gnl = get_next_line(fd, &line);
 		ft_putstr_fd(line, 1);
