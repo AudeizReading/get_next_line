26a74a0c3bd17dd613944892fefbf83f34eb102f
alellouc <alellouc@student.42nice.fr>
2021-05-31 11:19:11 +0200

    It is lunch time

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: alellouc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/12 08:54:45 by alellouc          #+#    #+#             */
-/*   Updated: 2021/05/31 10:29:50 by alellouc         ###   ########.fr       */
+/*   Updated: 2021/05/31 11:16:52 by alellouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,49 +16,6 @@
 #include <stdio.h>
 /* Fin debugage */
 
-/* Fonctions utilisées par méthode sv */
-/* char	*ft_strnew(size_t size)
-{
-	return (ft_calloc(sizeof(char), (size + 1)));
-}
-
-int		ft_memdel(void **ptr)
-{
-	unsigned int	i;
-
-	i = 0;
-	if (*ptr)
-	{
-		while (ptr[i])
-		{
-			ptr[i] = 0;
-		}
-		free(*ptr);
-		*ptr = NULL;
-		return (1);
-	}
-	return (0);
-}
-*char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	size_t	i;
-	char	*ptr;
-
-	i = 0;
-	if (!s || (long int)len < 0)
-		return (NULL);
-	ptr = (char *)malloc(len + 1);
-	if (ptr == NULL)
-		return (NULL);
-	while (start < ft_strlen(s) && i < len)
-	{
-		ptr[i] = s[start];
-		i++;
-		start++;
-	}
-	ptr[i] = 0;
-	return (ptr);
-}*/
 
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
@@ -102,7 +59,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 int	get_next_line(int fd, char **line)
 {
 /* 2e essai qui ne passe pas les testeurs */
-	int			ret;
+	/*int			ret;
 	static char		buf[BUFFER_SIZE + 1];
 	char	*newline;
 	char		*tmp;
@@ -137,32 +94,33 @@ int	get_next_line(int fd, char **line)
 			ft_memccpy(buf, test, '\0', BUFFER_SIZE);
 		ret = 1;
 	}
-	return (ret);
-
-/* Version sv */
-/*	ssize_t		r;
-	char	bf[BUFFER_SIZE + (r = 1)];
-	static char	*c_line = NULL;
-	char	*tmp;
+	return (ret);*/
+	int			ret;
+	static char	*buf;
+	char		*newline;
 
 	if (fd < 0 || !line || BUFFER_SIZE < 1)
 		return (-1);
-	c_line == NULL ? c_line = ft_strnew(0) : NULL;
-	while (!ft_strchr(c_line, '\n') && (r = read(fd, bf, BUFFER_SIZE)) > 0)
+	if (!buf)
+		buf = ft_calloc(sizeof(*buf), BUFFER_SIZE);
+	newline = ft_strdup("");
+	ret = 1;
+	while (!ft_strchr(buf, '\n') && ret > 0)
 	{
-		bf[r] = 0;
-		tmp = ft_strjoin(c_line, bf);
-		ft_memdel((void **)&c_line);
-		c_line = tmp;
+		printf("buf avant travail dessus: \033[31m%s\033[0m\n", buf);
+		if (newline)
+			printf("newline avant travail dessus: \033[31m%s\033[0m\n", newline);
+		newline = ft_strjoin(newline, buf);
+		ret = read(fd, buf, BUFFER_SIZE);
+		if (ret == -1)
+			return (-1);
+		buf[ret] = 0;
+		printf("buf apres travail dessus: \033[31m%s\033[0m\n", buf);
+		printf("newline apres travail dessus: \033[31m%s\033[0m\n", newline);
+		printf("\033[31m----------------------------------------------------------\033[0m\n");
 	}
-	if (r == 0)
-		*line = ft_strdup(c_line);
-	else if (r > 0)
-		*line = ft_substr(c_line, 0, (ft_strchr(c_line, '\n') - c_line));
+	if (ret == 0)
+		return (0);
 	else
-		return (-1);
-	tmp = ft_strdup(c_line + (ft_strlen(*line) + ((r > 0) ? +1 : +0)));
-	ft_memdel((void **)&c_line);
-	c_line = tmp;
-	return (r == 0 ? 0 * ft_memdel((void **)&c_line) : 1);*/
+		return (1);
 }
