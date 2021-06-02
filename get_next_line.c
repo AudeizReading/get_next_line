901d54441b01e90787e58bc986a5f9cfb6864ff9
alellouc <alellouc@student.42nice.fr>
2021-06-02 15:46:13 +0200

    Passed testers, now working on the real size to malloc/copy, and then will create new functions for resizing my gnl < 25 lines

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: alellouc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/12 08:54:45 by alellouc          #+#    #+#             */
-/*   Updated: 2021/06/01 18:35:55 by alellouc         ###   ########.fr       */
+/*   Updated: 2021/06/02 15:44:38 by alellouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,90 +58,42 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 int	get_next_line(int fd, char **line)
 {
-/* 2e essai qui ne passe pas les testeurs */
-	/*int			ret;
-	static char		buf[BUFFER_SIZE + 1];
-	char	*newline;
-	char		*tmp;
-	char		*test;
-
-	if (fd < 0 || !line || BUFFER_SIZE < 1)
-		return (-1);
-	ret = ft_strlen(buf) + 1;
-	newline = ft_strdup("");
-	while (ret > 0 && !ft_strchr(buf, '\n'))
-	{
-		if (!ft_strchr(buf, '\n'))
-		{
-			newline = ft_strjoin(newline, buf);
-			ret = read(fd, buf, BUFFER_SIZE);
-			if (ret < 0)
-				return (-1);
-			buf[ret] = 0;
-		}
-	}
-	if (!ret && !ft_strlen(newline))
-	{
-		free(newline);
-	}
-	else if (ret > 0 || ft_strlen(newline))
-	{
-		tmp = ft_calloc(sizeof(*tmp), ret + 1);
-		test = ft_memccpy(tmp, buf, '\n', ret);
-		*line = ft_strjoin(newline, tmp);
-		free(tmp);
-		if (test)
-			ft_memccpy(buf, test, '\0', BUFFER_SIZE);
-		ret = 1;
-	}
-	return (ret);*/
 	int			ret;
 	static char	buf[BUFFER_SIZE + 1];
-/*	static char	*buf;*/
 	char		*newline;
 	char		*tmp;
-/*	char		*after_line;*/
 
-	if (fd < 0 || !line || BUFFER_SIZE < 1)
+	if (read(fd, buf, 0) < 0 || !line || BUFFER_SIZE < 1)
 		return (-1);
-/*	if (!buf)
-		buf = ft_calloc(sizeof(*buf), BUFFER_SIZE + 1);*/
 	newline = ft_strdup("");
 	ret = ft_strlen(buf) + 1;
 	while (!ft_strchr(buf, '\n') && ret > 0)
 	{
-	/*	printf("buf avant travail dessus: \033[1;31m%s\033[0m\n", buf);
-		printf("newline avant travail dessus: \033[1;31m%s\033[0m\n", newline);*/
 		newline = ft_strjoin(newline, buf);
-	/*	if (buf)
-			free(buf);*/
 		ret = read(fd, buf, BUFFER_SIZE);
-		if (ret == -1)
+		if (ret == -1 || !newline)
 			return (-1);
 		buf[ret] = 0;
-/*		printf("buf apres travail dessus: \033[31m%s\033[0m\n", buf);
-		printf("newline apres travail dessus: \033[31m%s\033[0m\n", newline);
-		printf("\033[31m----------------------------------------------------------\033[0m\n");*/
 	}
-/*	printf("buf sortie boucle while: \033[33m%s\033[0m\n", buf);
-	printf("newline sortie boucle while: \033[33m%s\033[0m\n", newline);
-	printf("\033[33m----------------------------------------------------------\033[0m\n");*/
 	if (ft_strchr(buf, '\n'))
 	{
-		tmp = ft_calloc(sizeof(*tmp), ret);
-		ft_memccpy(buf, ft_memccpy(tmp, buf, '\n', ret), '\0', BUFFER_SIZE);
-	/*	after_line = ft_memccpy(tmp, buf, '\n', ret);*/
-	/*	printf("buf, avant affectation: \033[36m%s\033[0m\n", buf);
-		printf("tmp, apres affectation: \033[36m%s\033[0m\n", tmp);
-		printf("after_line, apres affectation: \033[36m%s\033[0m\n", after_line);*/
+		/*tmp = ft_calloc(sizeof(*tmp), ret);*/
+		tmp = ft_calloc(sizeof(*tmp), ft_strchr(buf, '\n') - buf);
+		if (!tmp)
+		{
+			free(newline);
+			return (-1);
+		}
+		ft_memccpy(buf, ft_memccpy(tmp, buf, '\n', ft_strchr(buf, '\n') - buf), '\0', ret - ft_strlen(ft_strchr(buf, '\n')));
 		newline = ft_strjoin(newline, tmp);
 		free(tmp);
-	/*	ft_memccpy(buf, after_line, '\0', BUFFER_SIZE);*/
-	/*	printf("newline, apres affectation: \033[36m%s\033[0m\n", newline);
-		printf("buf, apres affectation: \033[36m%s\033[0m\n", buf);
-		printf("\033[36m----------------------------------------------------------\033[0m\n");*/
 	}
 	*line = ft_strdup(newline);
+	if (!*line)
+	{
+		free(newline);
+		return (-1);
+	}
 	free(newline);
 	if (ret == 0)
 		return (0);
--- main_bonus.c
+++ main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: alellouc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/02 12:25:09 by alellouc          #+#    #+#             */
-/*   Updated: 2021/06/02 12:56:28 by alellouc         ###   ########.fr       */
+/*   Updated: 2021/06/02 14:37:28 by alellouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,124 +28,38 @@ int	main(int argc, char **argv)
 	size_t	fd2;
 	size_t	fd3;
 	int		gnl;
+	int		i = 0;
+	size_t fd[3];
 
 	if (argc == 4)
 	{
 		fd1 = open(argv[1], O_RDONLY);
+		fd[0] = fd1;
 		fd2 = open(argv[2], O_RDONLY);
+		fd[1] = fd2;
 		fd3 = open(argv[3], O_RDONLY);
+		fd[2] = fd3;
 	}
 	else
 	{
 		ft_putstr_fd("Il faut indiquer 3 noms de fichiers en paramètres, sans quoi le programme bonus ne se lancera pas\n", 1);
 		return (1);
 	}
-/*	while ((gnl = get_next_line(fd[i++], &line)) > 0)
+	while ((gnl = get_next_line(fd[i++], &line)) > 0)
 	{
-		ft_putstr_fd(line, 1);
-		ft_putstr_fd("\n", 1);
-		free(line);
-		if (i == 2)
+		if (i == 3)
 			i = 0;
-	}*/
-	gnl = get_next_line(fd1, &line);
+		printf("\n\033[%dm%s\033[0m\n", 31 + i, line);
+	
+	}
 	if (gnl == -1)
-		ft_putstr_fd("Votre gnl a rencontré un souci\n", 1);
-	ft_putstr_fd("Premiere ligne du fichier ", 1);
-	ft_putstr_fd(argv[1], 1);
-	ft_putstr_fd("\n\033[32m", 1);
-	ft_putstr_fd(line, 1);
-	ft_putstr_fd("\033[0m\n", 1);
-	free(line);
-
-	gnl = get_next_line(fd2, &line);
-	if (gnl == -1)
-		ft_putstr_fd("Votre gnl a rencontré un souci\n", 1);
-	ft_putstr_fd("Premiere ligne du fichier ", 1);
-	ft_putstr_fd(argv[2], 1);
-	ft_putstr_fd("\n\033[33m", 1);
-	ft_putstr_fd(line, 1);
-	ft_putstr_fd("\033[0m\n", 1);
-	free(line);
-
-	gnl = get_next_line(fd3, &line);
-	if (gnl == -1)
-		ft_putstr_fd("Votre gnl a rencontré un souci\n", 1);
-	ft_putstr_fd("Premiere ligne du fichier ", 1);
-	ft_putstr_fd(argv[3], 1);
-	ft_putstr_fd("\n\033[33m", 1);
-	ft_putstr_fd(line, 1);
-	ft_putstr_fd("\033[0m\n", 1);
-	free(line);
-
-	gnl = get_next_line(fd1, &line);
-	if (gnl == -1)
-		ft_putstr_fd("Votre gnl a rencontré un souci\n", 1);
-	ft_putstr_fd("Deuxieme ligne du fichier ", 1);
-	ft_putstr_fd(argv[1], 1);
-	ft_putstr_fd("\n\033[32m", 1);
-	ft_putstr_fd(line, 1);
-	ft_putstr_fd("\033[0m\n", 1);
-	free(line);
-
-	gnl = get_next_line(fd2, &line);
-	if (gnl == -1)
-		ft_putstr_fd("Votre gnl a rencontré un souci\n", 1);
-	ft_putstr_fd("Deuxieme ligne du fichier ", 1);
-	ft_putstr_fd(argv[2], 1);
-	ft_putstr_fd("\n\033[33m", 1);
-	ft_putstr_fd(line, 1);
-	ft_putstr_fd("\033[0m\n", 1);
-	free(line);
-
-	gnl = get_next_line(fd3, &line);
-	if (gnl == -1)
-		ft_putstr_fd("Votre gnl a rencontré un souci\n", 1);
-	ft_putstr_fd("Deuxieme ligne du fichier ", 1);
-	ft_putstr_fd(argv[3], 1);
-	ft_putstr_fd("\n\033[33m", 1);
-	ft_putstr_fd(line, 1);
-	ft_putstr_fd("\033[0m\n", 1);
-	free(line);
-
-	gnl = get_next_line(fd1, &line);
-	if (gnl == -1)
-		ft_putstr_fd("Votre gnl a rencontré un souci\n", 1);
-	ft_putstr_fd("Troisieme ligne du fichier ", 1);
-	ft_putstr_fd(argv[1], 1);
-	ft_putstr_fd("\n\033[32m", 1);
-	ft_putstr_fd(line, 1);
-	ft_putstr_fd("\033[0m\n", 1);
-	free(line);
-
-	gnl = get_next_line(fd2, &line);
-	if (gnl == -1)
-		ft_putstr_fd("Votre gnl a rencontré un souci\n", 1);
-	ft_putstr_fd("Troisieme ligne du fichier ", 1);
-	ft_putstr_fd(argv[2], 1);
-	ft_putstr_fd("\n\033[33m", 1);
-	ft_putstr_fd(line, 1);
-	ft_putstr_fd("\033[0m\n", 1);
-	free(line);
-
-	gnl = get_next_line(fd3, &line);
-	if (gnl == -1)
-		ft_putstr_fd("Votre gnl a rencontré un souci\n", 1);
-	ft_putstr_fd("Premiere ligne du fichier ", 1);
-	ft_putstr_fd(argv[3], 1);
-	ft_putstr_fd("\n\033[33m", 1);
-	ft_putstr_fd(line, 1);
-	ft_putstr_fd("\033[0m\n", 1);
-	free(line);
-
-/*	if (gnl == -1)
 		ft_putstr_fd("Votre gnl a rencontré un souci\n", 1);
 	if (line)
 	{
 		ft_putstr_fd(line, 1);
 		ft_putstr_fd("\n", 1);
 		free(line);
-	}*/
+	}
 	close(fd1);
 	close(fd2);
 	close(fd3);
