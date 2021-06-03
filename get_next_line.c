7aeb3dfe83bfbb0d63bab684b778a3a53858fff3
AudeizReading <aaericia@hotmail.fr>
2021-06-03 16:01:28 +0200

    Create a new function that parses the buffer et separate it into 2 strings if there is \n

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: alellouc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/12 08:54:45 by alellouc          #+#    #+#             */
-/*   Updated: 2021/06/02 15:51:16 by alellouc         ###   ########.fr       */
+/*   Updated: 2021/06/03 15:59:58 by alellouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,12 +56,42 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	return (dst);
 }
 
+char	*ft_parse_buf(size_t bytes, char *buf, char **newline)
+{
+	char	*end_line;
+	/*char	*next_line;*/
+	size_t	end_len;
+	size_t	begin_len;
+
+	/*next_line = ft_strchr(*buf, '\n');*/ /* voir si on malloc ca ou si on le supprime */
+	/*end_len = next_line - *buf;*/
+	end_len = ft_strchr(buf, '\n') - buf;
+	begin_len = bytes - end_len + 1;
+	end_line = ft_calloc(sizeof(*end_line), end_len);
+	if (!end_line)
+		return (NULL);
+/*	printf("\tbuffer avant travail dessus  : \033[33m%s\033[0m\n", buf);*/
+	ft_memccpy(buf, ft_memccpy(end_line, buf, '\n', end_len), '\0', begin_len);
+/*	ft_memccpy(buf, ft_memccpy(end_line, buf, '\n', end_len), '\0', bytes -
+**	ft_strlen(end_line));*/
+/*	printf("\tbegin_len : \033[36m%ld\033[0m\n", begin_len);
+	printf("\tbytes - ft_strlen(end_line) : \033[36m%ld\033[0m\n", bytes - ft_strlen(end_line));
+	printf("\tend_line apres travail dessus : \033[35m%s\033[0m\n", end_line);
+	printf("\tbuffer apres travail dessus : \033[35m%s\033[0m\n", buf);*/
+	/* free(next_line); */
+	*newline = ft_strjoin(*newline, end_line);
+/*	printf("\tnewline apres travail dessus : \033[35m%s\033[0m\n", *newline);*/
+	if (!*newline)
+		return (NULL);
+	free(end_line);
+	return (*newline);
+}
+
 int	get_next_line(int fd, char **line)
 {
 	int			ret;
 	static char	buf[BUFFER_SIZE + 1];
 	char		*newline;
-	char		*tmp;
 
 	if (read(fd, buf, 0) < 0 || !line || BUFFER_SIZE < 1)
 		return (-1);
@@ -76,17 +106,7 @@ int	get_next_line(int fd, char **line)
 		buf[ret] = 0;
 	}
 	if (ft_strchr(buf, '\n'))
-	{
-		tmp = ft_calloc(sizeof(*tmp), ft_strchr(buf, '\n') - buf);
-		if (!tmp)
-		{
-			free(newline);
-			return (-1);
-		}
-		ft_memccpy(buf, ft_memccpy(tmp, buf, '\n', ft_strchr(buf, '\n') - buf), '\0', ret - ft_strlen(ft_strchr(buf, '\n')));
-		newline = ft_strjoin(newline, tmp);
-		free(tmp);
-	}
+		newline = ft_parse_buf(ret, buf, &newline);
 	*line = ft_strdup(newline);
 	if (!*line)
 	{
